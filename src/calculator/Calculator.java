7f716678599b19bb7803d7f39122943fed35eeb1
davisklevgard <davisjklevgard@gmail.com>
2020-12-17 22:06:14 -0600

    believe there is something wrong with the label portion of the Calculator class. Have been working to fix that.

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -2,18 +2,48 @@ package calculator;
 
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 
 public class Calculator{
-    public static void main(String[] args) {
 
-    }
-
+    @FXML
+    private Label result;
+    private long number1 = 0;
+    private String operator ="";
+    private boolean start = true;
+    private Calculate calculate = new Calculate();
+    @FXML
     public void processNumbers(ActionEvent event){
-
+        if (start) {
+            result.setText("");
+            start = false;
+        }
+        String value = ((Button)event.getSource()).getText();
+        result.setText(result.getText() + value);
     }
 
+    @FXML
     public void processOperators(ActionEvent event){
+        String value = ((Button)event.getSource()).getText();
 
+        if (value.equals("=")) {
+            if (!operator.isEmpty())
+                return;
+
+            operator = value;
+            number1= Long.parseLong(result.getText());
+            result.setText("");
+        }else {
+            if (operator.isEmpty())
+                return;
+            long number2 = Long.parseLong(result.getText());
+            float output = calculate.calculate(number1, number2, operator);
+            result.setText(String.valueOf(output));
+            operator = "";
+            start = true;
+        }
     }
 
 
